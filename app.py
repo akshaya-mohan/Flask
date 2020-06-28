27c7de6dfbe202f7082712043b681dd4bb955bed
akshaya-mohan <53741275+akshaya-mohan@users.noreply.github.com>
2020-06-28 19:08:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os 
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT,jwt_required
@@ -9,16 +11,10 @@ from security import authenticate,identity
 from resources.store import Store,StoreList 
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db' #tells where data.db is
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 app.secret_key='asdfghjkl'
 api = Api(app)
-
-@app.before_first_request   #runs before the first request
-def create_tables():  #All the tables you need SQLAlchemy to create should be imported before this,only then it will create that table
-    db.create_all()  #creates data.db 
-
-
 
 jwt = JWT(app,authenticate,identity)
 
@@ -29,6 +25,7 @@ api.add_resource(StoreList,'/stores')
 api.add_resource(Register_user,'/register')
 
 if __name__ == "__main__":
+    from db import db
     db.init_app(app)   
     app.run(port=5000,debug=True)
 
